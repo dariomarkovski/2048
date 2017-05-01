f97af7a1b835e444bf05ae65ee5f1fdc3c102f6a
Dario Markovski <dario.markovski@outlook.com>
2017-05-01 22:50:37 +0200

    Added classes: State and Game

---
+++ 2048/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    public class Game
+    {
+        private State state;
+        private List<State> lastState;
+        private int numberAvailableUndos;
+        private string mode;
+
+        public Game()
+        {
+            state = new State();
+            lastState = new List<State>();
+            numberAvailableUndos = 2;
+        }
+
+        public Game(string mode)
+        {
+            if (mode.Equals("normal"))
+                numberAvailableUndos = 0;
+            else if (mode.Equals("ultimate"))
+                numberAvailableUndos = 1;
+        }
+
+        public string [][] getState()
+        {
+            return state.matrix;
+        }
+        public void updateState(string key)
+        {
+            if (numberAvailableUndos != 0)
+            {
+                if (lastState.Count == numberAvailableUndos) lastState.RemoveAt(0);
+                lastState.Add(new State(state));
+            }
+            state.move(key);
+        }
+        public void undoMove()
+        {
+            state = lastState[lastState.Count - 1];
+            lastState.RemoveAt(lastState.Count - 1);
+        }
+        public bool canUndo()
+        {
+            return lastState.Count > 0 ? true : false;
+        }
+    }
+}
---
+++ 2048/State.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    public class State
+    {
+        public string[][] matrix { get; set; }
+        private Random rnd;
+        public State()
+        {
+            matrix = new string[4][];
+            for(int i = 0; i < 4; i++)
+            {
+                matrix[i] = new string[4];
+                for(int j = 0; j < 4; j++)
+                {
+                    matrix[i][j] = "";
+                }
+            }
+            rnd = new Random();
+            generateRandom();
+        }
+        public State(State other)
+        {
+            matrix = new string[4][];
+            for (int i = 0; i < 4; i++)
+            {
+                matrix[i] = new string[4];
+                for (int j = 0; j < 4; j++)
+                {
+                    matrix[i][j] = other.matrix[i][j];
+                }
+            }
+            rnd = new Random();
+        }
+        private void generateRandom()
+        {
+            int counter = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (!(matrix[i][j].Equals("")))
+                        counter++;
+                }
+            }
+            if (counter == 16)
+            {
+                return;
+            }
+            while (true)
+            {
+                int row = rnd.Next(0, 4);
+                int col = rnd.Next(0, 4);
+                if (matrix[row][col].Equals(""))
+                {
+                    matrix[row][col] = "2";
+                    break;
+                }
+            }
+        }
+        public void move(string key) {
+            if (key.Equals("up"))
+            {
+                bool moved = false;
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (matrix[i][k].Equals(""))
+                        {
+                            for (int j = i + 1; j < 4; j++)
+                            {
+                                if (!matrix[j][k].Equals(""))
+                                {
+                                    matrix[i][k] = matrix[j][k];
+                                    matrix[j][k] = "";
+                                    moved = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (!matrix[i][k].Equals("") && matrix[i][k].Equals(matrix[i + 1][k]))
+                        {
+                            matrix[i][k] = (Convert.ToInt32(matrix[i][k]) * 2).ToString();
+                            matrix[i + 1][k] = "";
+                            for (int j = i + 1; j < 3; j++)
+                            {
+                                matrix[j][k] = matrix[j + 1][k];
+                                matrix[j + 1][k] = "";
+                            }
+                            moved = true;
+                        }
+                    }
+                }
+                if (moved) generateRandom();
+            }
+            if (key.Equals("down"))
+            {
+                bool moved = false;
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 3; i > 0; i--)
+                    {
+                        if (matrix[i][k].Equals(""))
+                        {
+                            for (int j = i - 1; j >= 0; j--)
+                            {
+                                if (!matrix[j][k].Equals(""))
+                                {
+                                    matrix[i][k] = matrix[j][k];
+                                    matrix[j][k] = "";
+                                    moved = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 3; i > 0; i--)
+                    {
+                        if (!matrix[i][k].Equals("") && matrix[i][k].Equals(matrix[i - 1][k]))
+                        {
+                            matrix[i][k] = (Convert.ToInt32(matrix[i][k]) * 2).ToString();
+                            matrix[i - 1][k] = "";
+                            for (int j = i - 1; j >= 1; j--)
+                            {
+                                matrix[j][k] = matrix[j - 1][k];
+                                matrix[j - 1][k] = "";
+                            }
+                            moved = true;
+                        }
+                    }
+                }
+                if (moved) generateRandom();
+            }
+            if (key.Equals("right"))
+            {
+                bool moved = false;
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 3; i > 0; i--)
+                    {
+                        if (matrix[k][i].Equals(""))
+                        {
+                            for (int j = i - 1; j >= 0; j--)
+                            {
+                                if (!matrix[k][j].Equals(""))
+                                {
+                                    matrix[k][i] = matrix[k][j];
+                                    matrix[k][j] = "";
+                                    moved = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 3; i > 0; i--)
+                    {
+                        if (!matrix[k][i].Equals("") && matrix[k][i].Equals(matrix[k][i - 1]))
+                        {
+                            matrix[k][i] = (Convert.ToInt32(matrix[k][i]) * 2).ToString();
+                            matrix[k][i - 1] = "";
+                            for (int j = i - 1; j >= 1; j--)
+                            {
+                                matrix[k][j] = matrix[k][j - 1];
+                                matrix[k][j - 1] = "";
+                            }
+                            moved = true;
+                        }
+                    }
+                }
+                if (moved) generateRandom();
+            }
+            if (key.Equals("left"))
+            {
+                bool moved = false;
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (matrix[k][i].Equals(""))
+                        {
+                            for (int j = i + 1; j < 4; j++)
+                            {
+                                if (!matrix[k][j].Equals(""))
+                                {
+                                    matrix[k][i] = matrix[k][j];
+                                    matrix[k][j] = "";
+                                    moved = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (!matrix[k][i].Equals("") && matrix[k][i].Equals(matrix[k][i + 1]))
+                        {
+                            matrix[k][i] = (Convert.ToInt32(matrix[k][i]) * 2).ToString();
+                            matrix[k][i + 1] = "";
+                            for (int j = i + 1; j < 3; j++)
+                            {
+                                matrix[k][j] = matrix[k][j + 1];
+                                matrix[k][j + 1] = "";
+                            }
+                            moved = true;
+                        }
+                    }
+                }
+                if (moved) generateRandom();
+            }
+        }
+        public void isFinished() { }
+    }
+}
